d189bd8abfa0a46efa929eb2c18d706adb8491a6
Davi Costa <davifncosta@gmail.com>
2023-07-16 21:06:19 -0300

    add typescript moduls and enum

---
+++ modulo2/enum/app.ts
@@ -0,0 +1,28 @@
+//Enuamerar os valores, mapeiam chaves para valores
+//Estruturas de dados não ordenadas
+/* 
+
+Enum TypeName{
+    constant1,
+    constant2,
+    constant3,
+}
+
+
+Por que usar ?
+- Facilidade em mudar valores
+- Reduz erro
+- Funciona somente em tempo de compilação
+- O tempo de execução será mais preciso e rapido
+- Permite criar constante
+*/
+
+// Numeric Enums
+enum Idiomas{
+    Portugues,
+    Frances,
+    Espanhol,
+    Ingles
+}
+
+console.log(Idiomas.Portugues)
