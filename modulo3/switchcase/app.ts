738217b29c7446bbd738de8ba7b7948eff208e00
Davi Costa <davifncosta@gmail.com>
2023-08-09 10:18:19 -0300

    add switch case

---
+++ modulo3/switchcase/app.ts
@@ -0,0 +1,13 @@
+let flor: string = "tulipa"
+
+switch(flor) {
+    case "rosa":
+        console.log("rosa")
+        break
+    case "tulipa":
+        console.log("tulipa")
+        break
+    default:
+        console.log("unknown flor: " + flor)
+        break
+}
