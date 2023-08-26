47ccde0f77d90454985809c9a7e268c9b27dbb3f
Davi Fernandes <davifncosta@gmail.com>
2023-08-25 21:08:24 -0300

    add exemple class

--- modulo5/classes.ts
+++ modulo5/classes.ts
@@ -5,37 +5,34 @@
 // Classes Aninhadas e Interfaces( Nested Class & Interface)
 
 class Pessoa {
-    nome: string;
-    idade: number;
-    aFazeres: string[];
-    constructor(nome: string, idade: number, aFazeres: string[]){
-        this.nome = nome;
-        this.idade = idade;
-        this.aFazeres = aFazeres;
-    }
-    
-    Cadastro(): string{
-        return `Seu nome ${this.nome}, sua idade ${this.idade}, seus afazeres ${this.aFazeres}`
-    }
+  nome: string;
+  idade: number;
+  aFazeres: string[];
+  constructor(nome: string, idade: number, aFazeres: string[]) {
+    this.nome = nome;
+    this.idade = idade;
+    this.aFazeres = aFazeres;
+  }
+
+  Cadastro(): string {
+    return `Seu nome ${this.nome}, sua idade ${this.idade}, seus afazeres ${this.aFazeres}`;
+  }
 }
 
-const george = new Pessoa("George", 24, ["Box", "Code"])
+const george = new Pessoa("George", 24, ["Box", "Code"]);
 
-console.log(george.Cadastro())
+console.log(george.Cadastro());
 
 class Estudante {
-    codigoEstudante;
-    nomeEstudante;
+  codigoEstudante: number;
+  nomeEstudante: string;
 
-    constructor() {
-        this.codigoEstudante = 0;
-        this.nomeEstudante = "";
-    }
+  constructor(codigoEstudante: number, nomeEstudante: string) {
+    this.codigoEstudante = codigoEstudante;
+    this.nomeEstudante = nomeEstudante;
+  }
 }
 
-const estudante = new Estudante();
-
-estudante.codigoEstudante = 190829; // Correto: removendo o zero à esquerda
-estudante.nomeEstudante = "Davi";
+const estudante = new Estudante( 10999, "Davi");;
 
 console.log(estudante);
